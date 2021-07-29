274fd48d12ac91afd0ffb25343e429ff33d2481b
Marte Olavsdatter <marte.olavsdatter@gmail.com>
2021-07-29 13:05:45 +0200

    Made nested object more logical

--- 06_Objekt/script.js
+++ 06_Objekt/script.js
@@ -1,28 +1,34 @@
 const person1 = ['Eline', 26, 'Barcelona', true];
 
 const person = {
-	navn: 'Eline',
-	alder: 26,
-	bosted: 'Barcelona',
-	student: true
+  navn: 'Eline',
+  alder: 26,
+  bosted: 'Barcelona',
+  student: true,
 };
 
-// Få tilgang til kun alder
 const objekt = {
-	nøkkel: 'verdi',
-	nøkkel1: 'verdi1',
-	nested: [{
-		nest1: 'underverdi',
-		id: 1
-	},
-	{
-		nest2: 'underverdi2',
-		id: 2
-	}],
+  navn: 'Eline',
+  alder: 26,
+  bosted: [
+    {
+      id: 1,
+      adresse: 'Liksomgata 23',
+      postnummer: '1234',
+      sted: 'Norge',
+    },
+    {
+      id: 2,
+      adresse: 'C/ Señores Curas 68',
+      postnummer: '12120',
+      sted: 'Castellón',
+    },
+  ],
 };
-console.log(objekt);
-console.log(objekt.nested)
-console.log(objekt.nested[1].nest2);
 
 console.log(person);
 console.log(person.alder);
+
+console.log(objekt);
+console.log(objekt.nested);
+console.log(objekt.bosted[1].adresse);
